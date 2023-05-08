7c57c2df6b060d2dec59cb19ee9df65aec70006d
World Free_Spirit <freespirit@localhost.localdomain>
2023-05-08 18:00:06 +0000

    Question fix

--- action.js
+++ action.js
@@ -1,32 +1,51 @@
 const loader = document.querySelector('.overlay');
 
-let dictionary = [];
-let wordToGuess;
-async function secretWord (){
-	const url = 'https://random-word-api.p.rapidapi.com/L/5';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '04bc19bd87mshd399b62baa34ec3p180442jsn9245eaaae5da',
-		'X-RapidAPI-Host': 'random-word-api.p.rapidapi.com'
-	}
-};
+let dictionary = ["apple", "beach", "chair", "dance", "earth", "fruit", "grape", "house", "igloo", "jolly",
+		"knife", "lemon", "mango", "navy", "ocean", "pearl", "queen", "radio", "sunny", "table",
+		"umbra", "vodka", "water", "xerox", "yacht", "zebra", "abuse", "badge", "chair", "dance",
+		"early", "flute", "grain", "horse", "igloo", "joker", "knock", "lemon", "mango", "night",
+		"oasis", "peace", "quilt", "radio", "sugar", "table", "uncle", "virus", "wreck", "xenon",
+		"yield", "zebra", "abide", "baker", "cable", "daisy", "elite", "frank", "glide", "hazel",
+		"issue", "jolly", "knead", "lemon", "mango", "noise", "olive", "peach", "quiet", "radar",
+		"siren", "tulip", "uncle", "voice", "world", "xerox", "yield", "zebra", "abuse", "bride",
+		"charm", "dance", "early", "flame", "grade", "house", "igloo", "joker", "knack", "lemon",
+		"mango", "novel", "ocean", "piano", "queen", "radio", "sunny", "table", "umbra", "vodka",
+		"water", "xenon", "yield", "zebra", "acorn", "bloom", "candy", "dream", "eager", "fable",
+		"glory", "hedge", "ivory", "jolly", "knock", "lemon", "mango", "night", "oasis", "proud",
+		"quail", "radio", "sugar", "table", "uncle", "virus", "wreck", "xerox", "yield", "zebra",
+		"abide", "beach", "chair", "dance", "early", "flute", "grain", "horse", "igloo", "joker",
+		"knack", "lemon", "mango", "noise", "olive", "peace", "quiet", "radar", "siren", "tulip",
+		"uncle", "vivid", "world", "xerox", "yield", "zebra", "abuse", "badge", "chair", "dance",
+		"early", "flame", "grape", "house", "igloo", "jolly", "knock", "lemon", "mango", "novel",
+		"ocean", "pearl", "quiet", "radar", "sugar", "table", "umbra", "vodka", "water", "xerox",
+		"yield", "zebra", "abide", "bloom", "candy", "dance", "early", "fable", "glory", "hazel",
+		"issue", "jolly", "knead", "lemonn"	  
+];
+let wordToGuess = dictionary[Math.floor(Math.random() * dictionary.length - 1)];
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	// dictionary = [...result];
-	wordToGuess = result;
-	getWord();
-} catch (error) {
-	console.error(error);
-}
-}
+// async function secretWord (){
+// 	const url = 'https://random-words5.p.rapidapi.com/getMultipleRandom?count=15&wordLength=5';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '038067ceaamshd28482b59046714p19a668jsn3602b0ecd893',
+// 		'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.json();
+// 	dictionary = [...result];
+// 	getWord();
+// } catch (error) {
+// 	console.error(error);
+// }
+// }
 
 // let getWord = () => {
 // 	if(dictionary.length === 0) return;
 // 	if (dictionary.length > 0) {
-// 		wordToGuess = dictionary[Math.floor(Math.random() * dictionary.length - 1)];
 // 		loader.style.display = 'none';
 // 		return wordToGuess;
 // 	}
@@ -115,8 +134,9 @@ let newLetter = (letter) => {
 	gameState.column++;
 };
 let purgeLetter = () => {
+	console.log('Purging')
 	if (gameState.column === 0) return;
-	gameState.column = '';
+gridBox[gameState.column].textContent = '';
 	gameState.column--;
 
 };
@@ -161,7 +181,7 @@ let getInput = () => {
 const gamestart = () => {
 	let game = document.querySelector('.game');
 	createGrid(game);
-	secretWord();
+	// secretWord();
 	getInput();
 };
 gamestart();
